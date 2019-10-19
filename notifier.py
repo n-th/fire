c1f3eb0719127a635a8e167ebeb630897290164a
karolinyoliveira <karolinyoliveira@usp.br>
2019-10-19 20:19:32 -0300

    atualizacao notifier

--- notifier.py
+++ notifier.py
@@ -3,8 +3,7 @@ from email.mime.text import MIMEText
 
 
 def notify(data,email):
-
-    # conexão com os servidores do google
+    # servidores do google
     host = 'smtp.gmail.com'
     port = 465
     # username ou email para logar no servidor
@@ -14,15 +13,14 @@ def notify(data,email):
     from_addr = 'spaceapps.saci@gmail.com'
     to_addrs = [email]
 
-    message = MIMEText('Hello World')
-    message['subject'] = 'Hello'
+    message = MIMEText('Atenção!\nIdentificamos um foco de incêndio em sua cidade na seguinte localização (%s, %s).\nPara mais informações, acesse: <link_maps>.\nDados obtidos via Nasa-FIRMS(Informações sobre incêndio para sistema de gerenciamento de recursos). \n\nEquipe Saci - Nasa Space Apps'% (data[0],data[1])) 
+    message['subject'] = 'Atenção cidade de %s-%s' % (data[2],data[3])
     message['from'] = from_addr
     message['to'] = ', '.join(to_addrs)
 
     # conectaremos de forma segura usando SSL
     server = smtplib.SMTP_SSL(host, port)
-    # para interagir com um servidor externo precisaremos
-    # fazer login nele
+    # login servidor externo
     server.login(username, password)
     server.sendmail(from_addr, to_addrs, message.as_string())
     server.quit()
