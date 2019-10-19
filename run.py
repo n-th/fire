926512557ccf942cb57022828fa5b238aa7a9acc
Nathalia Satie Gomazako <na.na.na@outlook.com>
2019-10-19 17:30:35 -0300

    teste

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ from notifier import notify
 if __name__=="__main__": 
     data = analyze("MODIS_C6_South_America_24h.csv")
 
-    emails = find()
+    emails = find() #teste
 
     notify(data, emails)
